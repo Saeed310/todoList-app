4817288bee4a2397272004f4c4cbf2a597a1f5c6
saeed <muhammedsaidsalih@gmail.com>
2023-08-22 20:17:08 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import TodoItems from './components/TodoItems/TodoItems';
+import AddItems from './components/AddItems/AddItems';
+class App extends Component {
+  state = {
+    items : [
+      {id:1,name:'Hamza',age:22},
+      {id:2,name:'Mohammed',age:23},
+      {id:3,name:'Abdo',age:24}
+    ]
+  }
+
+deleteItem = (id) => {
+  let items = this.state.items.filter(item => {
+    return item.id !==id
+  })
+  this.setState({items})
+
+}
+
+addItem = (item) => {
+  item.id = Math.random();
+  let items = this.state.items;
+  items.push(item);
+  this.setState({items})
+}
+
+
+  render() {
+    return(
+      <div className="App container">
+        <h1 className='text-center'> Todo List</h1> 
+        <TodoItems items={this.state.items} deleteItem={this.deleteItem} />
+        <AddItems addItem={this.addItem} />
+        
+      </div>
+    );
+  }
+}
+export default App;
